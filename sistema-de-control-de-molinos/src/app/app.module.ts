21eab366c72b7ba9b4fcab86771a748e5faf34ab
Jose Luis Muñoz Zuñiga <joseluismunozzuniga@gmail.com>
2020-06-20 16:34:08 -0400

    add compononets

--- sistema-de-control-de-molinos/src/app/app.module.ts
+++ sistema-de-control-de-molinos/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './components/cabecera/cabecera.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    FooterComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
