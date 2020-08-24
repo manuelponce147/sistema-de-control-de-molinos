7713106799c0c251a4a608f8f9791cfc0c7ecee6
Jose Luis Muñoz Zuñiga <joseluismunozzuniga@gmail.com>
2020-08-23 20:32:08 -0400

    users roles

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -19,6 +19,12 @@ export async function getUser(req:Request,res:Response){
     if(!user) return res.status(404).json({message:"No se ha encontrado el usuario"});
     return res.status(200).json(user);
 }
+export async function getUserData(req:Request,res:Response){
+    const {id} = req.params;
+    const user= await User.find().populate('name','email');
+    if(!user) return res.status(404).json({message:"No se ha encontrado el usuario"});
+    return res.status(200).json();
+}
 export async function changeRole(req:Request, res:Response){
     const {id}= req.params;
     const {userRole}=req.body;
--- backend/src/router/user.ts
+++ backend/src/router/user.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 const router = Router();
-import { getUsers, getUser, changeRole, getUsersName } from "../controllers/user.controller";
+import { getUsers, getUser, changeRole, getUsersName, getUserData } from "../controllers/user.controller";
 
 router.route('/users').get(getUsers);
 router.route('/usersInfo').get(getUsersName);
+router.route('/usersData').get(getUserData);
+
 
 router.route('/user/:id')
     .get(getUser)
