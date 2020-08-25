0129cc7887865c50187dc9a864e734b7b20b1df7
sebarohr <67083883+sebarohr@users.noreply.github.com>
2020-08-25 18:12:49 -0400

    Update

--- sistema-de-control-de-molinos/src/app/components/statistics/statistics.component.ts
+++ sistema-de-control-de-molinos/src/app/components/statistics/statistics.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PhotoService } from '../../services/photo.service';
 import { Producto } from '../../models/producto';
 import { PesajeService } from '../../services/pesaje.service';
+import { IfStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-statistics',
@@ -24,8 +25,19 @@ export class StatisticsComponent implements OnInit {
     });
 
   }
-  aplicar(value:string):void{
-    console.log(value);
+  aplicar(value:string,cov:string,mes:number):void{
+
+    for (let index = 0; index < this.pesajes.length; index++) {
+      if(this.pesajes[index].tipoTransaccion==cov){
+        if(this.pesajes[index].tipoProducto==value){
+          let dateP = new Date(this.pesajes[index].createdAt);
+          if(dateP.getMonth()+1 == mes){
+            console.log(this.pesajes[index]);
+          }
+        }
+      }
+
+    }
   }
 
 }
